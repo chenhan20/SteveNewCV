c9a9e91ae6d022aa4e613e1a174522836ff48681
chenhan20 <a0911558945@gmail.com>
2020-06-04 23:33:25 +0800

    20200604

--- pages/exerciseGrid.vue
+++ pages/exerciseGrid.vue
@@ -1,13 +1,54 @@
 <template>
   <v-app>
     <h1>{{ message }}</h1>
-    <div class="wrapper">
-      <div class="box a">a</div>
-      <div class="box b">b</div>
-      <div class="box c">c</div>
-      <div class="box d">d</div>
-      <div class="box e">e</div>
-      <div class="box f">f</div>
+    <div class="board">
+      <div class="wrapper">
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+        <span class="plot"></span>
+      </div>
+      <div id="garden" class style>
+        <div class="treatment carrot" style="grid-column-start: span 5">
+          <div class="bg"></div>
+        </div>
+      </div>
     </div>
   </v-app>
 </template>
@@ -23,24 +64,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.wrapper {
-  display: grid;
-  grid-template-columns: 100px 100px 100px;
-  grid-gap: 10px;
-  background-color: #fff;
-  color: #444;
-  .box {
-    background-color: #444;
-    color: #fff;
-    border-radius: 5px;
-    padding: 20px;
-    font-size: 150%;
-  }
-  .a {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 2;
+.board {
+  position: sticky;
+  top: 1em;
+  width: 50vw;
+  height: 50vw;
+  min-width: 300px;
+  min-height: 300px;
+  max-width: calc(100vh - 2em);
+  max-height: calc(100vh - 2em);
+  overflow: hidden;
+  border: 12px solid #6d5720;
+  border-radius: 12px;
+  .wrapper {
+    display: grid;
+    grid-template-columns: 20% 20% 20% 20% 20%;
+    grid-template-rows: 20% 20% 20% 20% 20%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    .plot {
+      z-index: 50;
+      background: none;
+      box-shadow: inset 2px 2px 0 rgba(255, 255, 255, 0.05),
+        inset -2px -2px 0 #665235;
+    }
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,35 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md8
-    >
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm6 md6>
       <div class="text-center">
-        <img
-          src="/steveNewImage.png"
-        >
+        <img src="/steveNewImage.png" />
         <v-divider></v-divider>
       </div>
-        <blockquote class="blockquote">
-          <footer>
+      <blockquote class="blockquote">
+        <footer>
           <div>Make one</div>
           <div>problem solver</div>
           <div>creater</div>
           <div>innovator</div>
           <div>not a programmer</div>
-            <small>
-              <em>&mdash;Joma Tech</em>
-            </small>
-          </footer>
+          <small>
+            <em>&mdash;Joma Tech</em>
+          </small>
+        </footer>
       </blockquote>
-      <v-card>
-      </v-card>
+      <v-card></v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
 
 export default {
   components: {
     Logo,
     VuetifyLogo
   }
-}
+};
 </script>
