5c6fc356aaf2ede4b03a0b78bdefa5f542473958
chenhan20 <a0911558945@gmail.com>
2020-10-14 21:50:35 +0800

    loaddata

--- pages/stock.vue
+++ pages/stock.vue
@@ -1,27 +1,68 @@
 <template>
-    <h1>{{ head.title }}</h1>
+  <!-- <h1>{{ head.title }}</h1> -->
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4" v-for="(stock, i) in stockList" :key="i">
+        {{ stock.name }} 
+        {{ stock.price }} 
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import {createStore} from 'vuex'
+import { createStore } from "vuex";
 
 export default {
   data: () => ({
     head: {
       title: "STOCK",
-    }
+    },
+    stockList: [],
   }),
-    created: function () {
+  created: function () {
     // `this` points to the vm instance
-    console.log('a is: ')
+    console.log("a is: ");
+    this.stockList = stockData.stockList;
   },
   methods: {
-    getStock: function(event) {
-        
-    }
-  }
+    getStock: function (event) {},
+  },
 };
 
+const stockData = {
+  stockList: [
+    { name: "NKE", price: 129.27 },
+    { name: "ORCL", price: 61.11 },
+    { name: "WORK", price: 32.57 },
+    { name: "PTON", price: 131.74 },
+    { name: "SNE", price: 74.36 },
+    { name: "WMT", price: 145.7 },
+    { name: "CRM", price: 268.74 },
+    { name: "LMT", price: 389.43 },
+    { name: "VFC", price: 76.26 },
+    { name: "ASML", price: 404.81 },
+    { name: "GPRO", price: 6.77 },
+    { name: "DIS", price: 128.11 },
+    { name: "COST", price: 381.04 },
+    { name: "AMZN", price: 3454.5 },
+    { name: "FB", price: 277.78 },
+    { name: "ZM", price: 509.1 },
+    { name: "SPOT", price: 264.32 },
+    { name: "NVDA", price: 572.68 },
+    { name: "INTC", price: 54.37 },
+    { name: "AMD", price: 84.96 },
+    { name: "BRK-B", price: 210.97 },
+    { name: "GOOG", price: 1581.04 },
+    { name: "AAPL", price: 121.76 },
+    { name: "UBER", price: 35.84 },
+    { name: "TSM", price: 89.31 },
+    { name: "UMC", price: 5.55 },
+    { name: "MU", price: 52.03 },
+    { name: "TSLA", price: 456.33 },
+    { name: "NFLX", price: 568.41 },
+  ],
+};
 </script>
 
 <style lang="scss" scoped>
